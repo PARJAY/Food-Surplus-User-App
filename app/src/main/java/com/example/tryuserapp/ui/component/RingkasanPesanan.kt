783931d3d04df3ed2018084a741d71ec04ffb8a1
SirUaee <gilang.okandhewanta@gmail.com>
2024-04-23 18:02:55 +0800

    Ringkasan Pesanan Berhasil

--- app/src/main/java/com/example/tryuserapp/ui/component/RingkasanPesanan.kt
+++ app/src/main/java/com/example/tryuserapp/ui/component/RingkasanPesanan.kt
@@ -11,10 +11,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -22,59 +29,76 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tryuserapp.presentation.katalis_screen.SelectedKatalis
 import com.example.tryuserapp.ui.theme.Orange
 import com.example.tryuserapp.ui.theme.TryUserAppTheme
 
 @Composable
-fun RingkasanPesanan(){
-    Column(
+fun RingkasanPesanan(
+    selectedKatalis: SnapshotStateList<SelectedKatalis>
+) {
+
+    var totalHarga = 0F
+
+    selectedKatalis.forEach {
+        totalHarga += (it.hargaKatalis * it.quantity)
+    }
+
+
+
+    LazyColumn(
         modifier = Modifier
             .border(
                 BorderStroke(1.dp, Color.Black),
                 shape = RoundedCornerShape(16.dp)
             )
             .padding(16.dp)
-    ){
-        Text(
-            text = "Ringkasan Pesanan",
-            style = TextStyle(
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold
-            )
+    ) {
+        item {
+            Column() {
+                Text(
+                    text = "Ringkasan Pesanan",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
 
-        )
-        Spacer(modifier = Modifier.size(width = 0.dp, height = 8.dp))
-        Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween) {
-            Text(text = "Capcay "+ "x2")
-            Text(text = "Rp.20.000")
+                )
+                Spacer(modifier = Modifier.size(width = 0.dp, height = 8.dp))
+
+            }
         }
-        Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween) {
-            Text(text = "Ayam Goreng "+ "x1")
-            Text(text = "Rp.10.000")
+
+
+        items(selectedKatalis) {
+            Column {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = it.namaKatalis + " x " +  it.quantity )
+                    Text(text = "Rp." + (it.hargaKatalis* it.quantity ))
+                }
+
+            }
+
         }
-        Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween) {
-            Text(text = "Ongkir ")
-            Text(text = "Rp.15.000")
+        item {
+            Text(text = "Total Harga $totalHarga")
+
         }
-        Spacer(modifier = Modifier.size(width = 0.dp, height = 50.dp))
-        Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween) {
-            Text(text = "Total Pembayaran ")
-            Text(text = "Rp.45.000")
-        }
+
     }
 }
 
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun RingkasanPesananPreview(){
-    TryUserAppTheme {
-        Surface {
-            RingkasanPesanan()
-        }
-    }
-}
+
+//@Preview(showBackground = true)
+//@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+//@Composable
+//fun RingkasanPesananPreview(){
+//    TryUserAppTheme {
+//        Surface {
+//            RingkasanPesanan(selectedKatalis = SnapshotStateList<SelectedKatalis>)
+//        }
+//    }
+//}
